8d4686c022b797c57d536eb367d51c89c568cce2
Van Le <singularity@vannio.co.uk>
2016-06-22 16:50:06 +0100

    Added CSS file

--- src/controller.js
+++ src/controller.js
@@ -1,35 +1,38 @@
-var thermostat = new Thermostat();
+$(document).ready(function(){
+  var thermostat = new Thermostat();
 
-function updateTemperatureText() {
-  $('#output-temperature').text(thermostat.temperature);
-};
+  function updateTemperatureText() {
+    $('#output-temperature').text(thermostat.temperature);
+    $('#output-temperature').attr('class', thermostat.energyConsumption());
+  };
 
-updateTemperatureText();
-
-function updatePowerSavingMode() {
-  var powerSavingMode =  thermostat.isPowerSavingOn ? "On" : "Off";
-  $('#output-powersaving').text(powerSavingMode);
-};
-
-updatePowerSavingMode();
-
-$('#input-increase').on('click', function() {
-  thermostat.increaseTemperature();
   updateTemperatureText();
-});
 
-$('#input-decrease').on('click', function() {
-  thermostat.decreaseTemperature();
-  updateTemperatureText();
-});
+  function updatePowerSavingMode() {
+    var powerSavingMode =  thermostat.isPowerSavingOn ? "On" : "Off";
+    $('#output-powersaving').text(powerSavingMode);
+  };
 
-$('#input-toggle').on('click', function() {
-  thermostat.togglePowerSavingMode();
   updatePowerSavingMode();
-  updateTemperatureText();
-});
 
-$('#input-reset').on('click', function() {
-  thermostat.reset();
-  updateTemperatureText();
+  $('#input-increase').on('click', function() {
+    thermostat.increaseTemperature();
+    updateTemperatureText();
+  });
+
+  $('#input-decrease').on('click', function() {
+    thermostat.decreaseTemperature();
+    updateTemperatureText();
+  });
+
+  $('#input-toggle').on('click', function() {
+    thermostat.togglePowerSavingMode();
+    updatePowerSavingMode();
+    updateTemperatureText();
+  });
+
+  $('#input-reset').on('click', function() {
+    thermostat.reset();
+    updateTemperatureText();
+  });
 });
