0dd69ca4296f187f0a6976ac9fd73a49d1347430
riyapabari <riya.pabari@gmail.com>
2016-06-22 11:43:14 +0100

    added increaseTemperature function, and default & manual settings

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,11 +1,47 @@
 describe('Thermostat', function(){
   var thermostat;
 
-  beforeEach(function(){
-    thermostat = new Thermostat();
-  });
+  describe('default settings', function() {
 
-  it('starts at 20oC', function(){
-    expect(thermostat.temperature).toEqual(20);
-  });
+  	beforeEach(function(){
+    	thermostat = new Thermostat();
+  	});
+
+	  it('starts at DEFAULT_TEMPERATURE', function(){
+	    expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE);
+	  });
+
+	  describe('#increaseTemperature', function() {
+
+	  	it('should increase the temperature by DEFAULT_INCREMENT', function() {
+	  		thermostat.increaseTemperature();
+	  		var expectedTemperature = thermostat.DEFAULT_TEMPERATURE + thermostat.DEFAULT_INCREMENT;
+	  		expect(thermostat.temperature).toEqual(expectedTemperature);
+	  	});
+	  	
+	  });
+
+	});
+
+	describe('manual settings', function() {
+
+		beforeEach(function(){
+    	thermostat = new Thermostat(15, 1);
+  	});
+
+		it('starts at 15oC', function(){
+	    expect(thermostat.temperature).toEqual(15);
+	  });
+
+		describe('#increaseTemperature', function() {
+
+	  	it('should increase the temperature by 1', function() {
+	  		thermostat.increaseTemperature();
+	  		expect(thermostat.temperature).toEqual(16);
+	  	});
+	  	
+	  });
+
+	});
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,11 @@
-var Thermostat = function(){
-  this.temperature = 20;
+var Thermostat = function(startTemperature, increment){
+	this.DEFAULT_TEMPERATURE = startTemperature || 20;
+	this.DEFAULT_INCREMENT = increment || 0.5;
+  this.temperature = this.DEFAULT_TEMPERATURE;
+};
+
+Thermostat.prototype = {
+	increaseTemperature: function() {
+		this.temperature += this.DEFAULT_INCREMENT;
+	}
 };
