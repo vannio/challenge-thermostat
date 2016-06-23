524e52971cb1acd9d0343001732ac52cfc8000a1
Van Le <singularity@vannio.co.uk>
2016-06-23 15:01:35 +0100

    Added dropdown list to select different locations/cities

--- src/script.js
+++ src/script.js
@@ -33,6 +33,44 @@ $ (document).ready(function(){
     $('#power-saving-status').text(string);
   };
   function UpdatesEnergy(){
-    $("div").attr('class', thermostat.energyUsage());
+    $("#energy-usage").attr('class', thermostat.energyUsage());
   };
+
+  $('#location-form').on('submit', function(event){
+    event.preventDefault();
+    var query = $(event.currentTarget).serializeArray();
+    var autocompleteUrl = "http://autocomplete.wunderground.com/aq";
+
+    $.ajax({
+      url: autocompleteUrl,
+      jsonp: "cb",
+      dataType: "jsonp",
+      data: {
+        query: query[0].value,
+        format: "json"
+      },
+      success: function(response){
+        var data = response.RESULTS;
+        $("#location-select select").empty();
+
+        for (var i = 0; i < data.length; i++){
+          $("#location-select select").append("<option value=" + data[i].l + ">" + data[i].name + "</option>")
+        }
+      }
+    });
+
+  });
+
+  $('#location-select').on('submit', function(event){
+    event.preventDefault();
+    var query = $(event.currentTarget).serializeArray();
+    var url = "http://api.wunderground.com/api/" + wundergroundID + "/conditions/" + query[0].value + ".json";
+
+    $.get(url, function(data){
+      var weather = data.current_observation.weather;
+      var temperature = data.current_observation.feelslike_c;
+      $('#weather').text(weather + ", " + temperature);
+    });
+  });
+
 });
